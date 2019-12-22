0015846a485634fa6efb9072aea715bd062cbca5
Ellyll <cawsllyffant-git@yahoo.com>
2019-12-22 17:16:06 +0000

    Make tree and star

--- main.js
+++ main.js
@@ -66,96 +66,118 @@
         }
     }
 
+    /**
+     * 
+     * @param {number} degrees
+     * @returns {number}
+     */
+    function degToRad(degrees) {
+        return degrees * ((Math.PI*2) / 360.0);
+    }
+
 
     /**
      * @param {CanvasRenderingContext2D} context
      * @param {string} instructions
+     * @param {number} angleStep
+     * @param {number} distance
+     * @param {string | CanvasGradient | CanvasPattern} colour
+     * @param {boolean} shouldDrawLights
      */
-    function draw(context, instructions) {
+    function draw(context, instructions, angleStep, distance, colour, shouldDrawLights) {
 
-        const initialLocation = new Location(context.canvas.width/2, context.canvas.height, Math.PI);
+        const initialLocation = new Location(context.canvas.width/2, context.canvas.height/1.1, Math.PI);
         context.beginPath();
+        context.strokeStyle = colour;
         context.moveTo(initialLocation.x, initialLocation.y);
+
+        const colours = [ 'red', 'yellow', 'blue', 'purple', 'magenta', 'cyan' ];
+        let colourIndex = 0;
 
         let state = new State(initialLocation, [], instructions);
         for (let i=0 ; i<instructions.length ; i++) {
-            let d = 0.04;
-            let newX = state.currentLocation.x + d*Math.sin(state.currentLocation.angle);
-            let newY = state.currentLocation.y + d*Math.cos(state.currentLocation.angle);
+            let newX = state.currentLocation.x + distance*Math.sin(state.currentLocation.angle);
+            let newY = state.currentLocation.y + distance*Math.cos(state.currentLocation.angle);
             let newAngle = state.currentLocation.angle;
+            let oldFillStyle;
 
             switch(instructions.substr(i,1)) {
-                case '0':
+                case 'F':
                     context.lineTo(newX, newY);
-                    state = new State(new Location(newX, newY, newAngle), state.history, instructions);
-                    //TODO: Draw leaf
-                    break;
-                case '1':
-                    context.lineTo(newX, newY);
-                    state = new State(new Location(newX, newY, newAngle), state.history, instructions);
+                    context.stroke();
+                    if (shouldDrawLights && Math.random() >= 0.95) {
+                        oldFillStyle = context.fillStyle;
+                        context.beginPath();
+                        context.fillStyle = colours[colourIndex % colours.length];
+                        colourIndex++;
+                        context.arc(newX, newY, 3, 0, Math.PI*2);
+                        context.fill();
+                        context.fillStyle = oldFillStyle;
+                    }
+                    state = new State(new Location(newX, newY, newAngle), state.history, instructions);                    
                     break;
                 case '[':
                     state.history.push(Object.assign({}, state.currentLocation));
-                    state.currentLocation.angle = state.currentLocation.angle - Math.PI/4;
                     break;
                 case ']':
                     state.currentLocation = state.history.pop();
-                    state.currentLocation.angle = state.currentLocation.angle + Math.PI/4;
+                    context.beginPath();
                     context.moveTo(state.currentLocation.x, state.currentLocation.y);
+                    break;
+                case '+':
+                    state.currentLocation.angle = state.currentLocation.angle + angleStep;
+                    break;
+                case '-':
+                    state.currentLocation.angle = state.currentLocation.angle - angleStep;
                     break;
                 default:
                     throw Error("Invalid instruction:" + instructions.substr(i,1) + " at " + i);
             }
         }
-        context.stroke();
-        
-        // const positions = [ { x: context.canvas.width/2, y: 0, angle: Math.PI/2 } ];
-        // context.beginPath();
-        // context.moveTo(positions[0].x, positions[0].y);
-        // for (let i=0 ; i<state.length ; i++) {
-        //     switch(state[i]) {
-        //         case '0':
-        //             let oldPos = positions[positions.length-1];
-        //             let newPos = {
-        //                 x: oldPos.x + 5*Math.sin(oldPos.angle),
-        //                 y: oldPos.y + 5*Math.cos(oldPos.angle),
-        //                 angle: oldPos.angle
-        //             }
-        //             context.lineTo(newPos.x, newPos.y);
-        //             positions.push(newPos);
-        //         case '1':
-        //             let oldPos = positions[positions.length-1];
-        //             let newPos = {
-        //                 x: oldPos.x + 5*Math.sin(oldPos.angle),
-        //                 y: oldPos.y + 5*Math.cos(oldPos.angle),
-        //                 angle: oldPos.angle
-        //             }
-        //             context.lineTo(newPos.x, newPos.y);
-        //             positions.push(newPos);
-            
-        //     }
-        // }
+    }
+
+    /**
+     * 
+     * @param {HTMLCanvasElement} canvas 
+     */
+    function maximise(canvas) {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
     }
 
 
     document.addEventListener('DOMContentLoaded', function() {
         const { canvas, context } = getCanvasAndContext('canvas');
-        context.beginPath();
-        // context.moveTo(canvas.width/2, canvas.height/2);
-        // context.lineTo(200, 200);
-        // context.stroke();
+        maximise(canvas);
 
-        const alphabet = { variables: [ '0', '1' ], constants: [ '[', ']' ] };
-        const start = [ '0' ];
+
+        // Tree
+        const alphabet = { variables: [ 'F' ], constants: '[]-+'.split('') };
+        const start = 'F'.split('');
         const rules = {
-            '1': ['1', '1'],
-            '0': ['1', '[', '0', ']', '0' ]
+           'F': 'F[+FF][-FF]F[-F][+F]F'.split('')
         };
         const grammar = new Grammar(alphabet, start, rules);
+        const angleStep = degToRad(69/4); // degToRad(36);
+        const distance = 10;
+        const instructions = runGrammar(4, grammar);
+        context.rotate(Math.PI);
+        context.translate(-canvas.width, -canvas.height);
+        draw(context, instructions, angleStep, distance, 'rgb(5, 68, 13)', true);
+        context.resetTransform();
 
-
-        const instructions = runGrammar(14, grammar);
-        console.log({instructions});
-        draw(context, instructions);
+        // Star
+        const starStart = 'F'.split('');
+        const starRules = {
+           'F': 'F++F'.split('')
+        };
+        const starGrammar = new Grammar(alphabet, starStart, starRules);
+        const starAngleStep = degToRad(99); // degToRad(36);
+        const starDistance = 90;
+        const starInstructions = runGrammar(7, starGrammar);
+        const starColour = 'yellow';
+        context.rotate(Math.PI);
+        context.translate(-(canvas.width+6), -(canvas.height-74));
+        draw(context, starInstructions, starAngleStep, starDistance, starColour, false);
     });
 })();
